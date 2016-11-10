58697b603712b9cad113072d85dcca30041113c0
ramseyboy <awahannan@gmail.com>
2016-11-10 16:53:18 -0500

    Handle arrow keys

--- DungeonGenerator/src/main.cpp
+++ DungeonGenerator/src/main.cpp
@@ -1,11 +1,31 @@
+#include <iostream>
 #include "libtcod.hpp"
 
 int main() {
-    TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
-    while ( !TCODConsole::isWindowClosed() ) {
-        TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,NULL,NULL);
+    int playerx = 40,playery = 25;
+    TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);
+    while (!TCODConsole::isWindowClosed()) {
+        TCOD_key_t key;
+        TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
+        switch(key.vk) {
+            case TCODK_UP:
+                playery--;
+                break;
+            case TCODK_DOWN:
+                playery++;
+                break;
+            case TCODK_LEFT:
+                playerx--;
+                break;
+            case TCODK_RIGHT:
+                playerx++;
+                break;
+            default:
+                break;
+                //std::cout << "key pressed " << key.vk;
+        }
         TCODConsole::root->clear();
-        TCODConsole::root->putChar(40,25,'@');
+        TCODConsole::root->putChar(playerx, playery, '@');
         TCODConsole::flush();
     }
     return 0;
